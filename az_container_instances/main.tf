d45dccfad245b37b68855d71e31b7ccaddd8d302
Bruna de Moraes Sousa <ninhafiadedeus@gmail.com>
2024-09-05 02:43:52 -0300

    readiness and liveness probess add

--- az_container_instances/main.tf
+++ az_container_instances/main.tf
@@ -27,6 +27,15 @@ resource "azurerm_container_group" "acigroup" {
   ip_address_type = "Public"
   dns_name_label = "azterraformaci"
   os_type = "Linux"
+  # restart_policy = "Always" is the default ( values are: Always, Never, OnFailure) 
+
+  # tell ACI to send all the logs to Log Analytics
+  diagnostics {
+    log_analytics {
+      workspace_id = azurerm_log_analytics_workspace.wploganalytics.workspace_id
+      workspace_key = azurerm_log_analytics_workspace.wploganalytics.primary_shared_key
+    }
+  }
 
 # handle with ACR authentication 
   image_registry_credential {
@@ -62,9 +71,41 @@ resource "azurerm_container_group" "acigroup" {
       storage_account_key = azurerm_storage_account.storageact.primary_access_key
       storage_account_name = azurerm_storage_account.storageact.name
     }
+
+    # checks if the application is ready to accept incoming traffic after it completes a restart process.
+    readiness_probe {
+      exec = ["cat","/tmp/healthy"]
+      initial_delay_seconds = 2
+      period_seconds = 70 # How often (in seconds) to perform the probe
+      failure_threshold = 3 # How many times to try the probe before restarting the container (liveness probe) or marking the container as unhealthy (readiness probe)
+      success_threshold = 1 # Minimum consecutive successes for the probe to be considered successful after having failed
+      timeout_seconds = 40 # Number of seconds after which the probe times out
+    }
+
+    liveness_probe {
+      exec = ["cat","/tmp/healthy"]
+      initial_delay_seconds = 2
+      period_seconds = 60
+      failure_threshold = 3
+      success_threshold = 1
+      timeout_seconds = 40
+    }
   }
 
   tags = {
     environment = "dev"
   }
+
+}
+
+##################################
+# AZURE LOG ANALYTICS BLOCK
+# > add a workspace
+##################################
+resource "azurerm_log_analytics_workspace" "wploganalytics" {
+  name = "ch04storagelogs"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku = "PerGB2018"
+  retention_in_days = 30
 }
--- az_container_instances/variables.tf
+++ az_container_instances/variables.tf
@@ -1,7 +1,7 @@
 # variables used to access the ACR configured in az_container_registry directory
 variable "acrimage" {
   type    = string
-  default = "ch03/image01:v1"
+  default = "devox:v2"
 }
 
 variable "acruser" {
